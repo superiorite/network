c483c81e4ed5ab0e6711c225acca79c128d798ea
Vladislav <superiorite.vladislav@gmail.com>
2020-03-29 11:54:13 +0500

    Добавлено удаление пользователя и профиля

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -132,4 +132,22 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   GET api/profile
+// @desc    Delete profile, user & posts
+// @acces   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove posts
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
